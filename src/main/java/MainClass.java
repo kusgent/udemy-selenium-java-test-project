d4ad8400adb4d26a122944dd9a06c2cee309d256
Kustov Ilya <kusgent@yandex.ru>
2022-01-01 23:06:18 +0300

    Initial commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.close();
+    }
+}
