896c207350854021e6619573abee65b2dd17639d
Kustov Ilya <kusgent@yandex.ru>
2022-01-04 14:49:40 +0300

    Study basic selenium methods

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -5,7 +6,18 @@ public class MainClass {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().setSize(new Dimension(900, 500));
+
         driver.get("https://www.google.com/");
-        driver.close();
+        driver.navigate().to("https://yandex.ru/");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+        driver.quit();
     }
 }
