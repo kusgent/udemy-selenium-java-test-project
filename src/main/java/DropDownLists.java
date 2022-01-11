521b56715df66ba1032a1eefcf8f91a5031303ab
Kustov Ilya <kusgent@yandex.ru>
2022-01-11 22:31:51 +0300

    Explore work with drop-down lists

---
+++ src/main/java/DropDownLists.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class DropDownLists {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("http://the-internet.herokuapp.com/dropdown");
+        driver.findElement(By.xpath("//select[@id='dropdown']")).click();
+        driver.findElement(By.xpath("//select[@id='dropdown']/option[3]")).click();
+
+        driver.quit();
+    }
+}
