0283961fe82f022a3a017021e7b3f54d34365760
Kustov Ilya <kusgent@yandex.ru>
2022-01-16 16:20:14 +0300

    Work with alerts

---
+++ src/main/java/Alerts.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Alerts {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("http://the-internet.herokuapp.com/javascript_alerts");
+
+        driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.switchTo().alert().accept();
+
+        driver.findElement(By.xpath("//button[text()='Click for JS Confirm']")).click();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.switchTo().alert().dismiss();
+
+        driver.findElement(By.xpath("//button[text()='Click for JS Prompt']")).click();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Alert alert = driver.switchTo().alert();
+        alert.sendKeys("Hello!");
+        alert.accept();
+
+        driver.quit();
+    }
+}
