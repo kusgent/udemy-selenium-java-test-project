13d85d83bdbbb120a51d3f1f0419b4e5f8325add
Kustov Ilya <kusgent@yandex.ru>
2022-01-16 14:20:38 +0300

    Execution Javascript

---
+++ src/main/java/ExecJavascript.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class ExecJavascript {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("http://en.wikipedia.org");
+
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0, 1000)", "");
+        jse.executeScript("window.scrollBy(0, -1000)", "");
+//        jse.executeScript("alert('Hello world!');");
+
+        driver.quit();
+    }
+}
