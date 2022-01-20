2d553a234d1bc74afa274bdb5252cca3f93e5b91
Kustov Ilya <kusgent@yandex.ru>
2022-01-20 23:18:39 +0300

    Create screenshots

---
+++ src/main/java/CreateScreenshots.java
@@ -0,0 +1,39 @@
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+
+public class CreateScreenshots {
+    static WebDriver driver;
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("http://en.wikipedia.org/");
+
+        WebElement searchInput = driver.findElement(By.xpath("//input[@type='search']"));
+        searchInput.sendKeys("test text");
+        searchInput.sendKeys(Keys.ENTER);
+
+        Date dateNow = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("hh_mm_ss");
+        String fileName = format.format(dateNow) + ".png";
+
+        File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(screenshot, new File("C:\\Screenshots\\" + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        driver.quit();
+    }
+}
