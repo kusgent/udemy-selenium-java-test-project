e8e2dbed32dfe3b1e8b4e71fc365e5e4a307dc03
Kustov Ilya <kusgent@yandex.ru>
2022-01-22 17:02:52 +0300

    Running tests without opening a browser

--- src/main/java/UploadFiles.java
+++ src/main/java/UploadFiles.java
@@ -1,10 +1,18 @@
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
 public class UploadFiles {
     static WebDriver driver;
@@ -12,10 +20,12 @@ public class UploadFiles {
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
         System.setProperty("webdriver.gecko.driver", "C:\\geckodriver\\geckodriver.exe");
         System.setProperty("webdriver.edge.driver", "C:\\edgedriver_win64\\msedgedriver.exe");
+        System.setProperty("phantomjs.binary.path", "C:\\Users\\kusge\\Downloads\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe");
 
 //        driver = new ChromeDriver();
 //        driver = new FirefoxDriver();
-        driver = new EdgeDriver();
+//        driver = new EdgeDriver();
+        driver = new PhantomJSDriver();
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
@@ -23,6 +33,17 @@ public class UploadFiles {
         driver.get("http://the-internet.herokuapp.com/upload");
         driver.findElement(By.xpath("//input[@type='file']")).sendKeys("C:\\Users\\kusge\\Downloads\\Screenshot_2.png");
         driver.findElement(By.xpath("//input[@id='file-submit']")).click();
+
+        Date dateNow = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("hh_mm_ss");
+        String fileName = format.format(dateNow) + ".png";
+
+        File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(screenshot, new File("C:\\Screenshots\\" + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         driver.quit();
     }
