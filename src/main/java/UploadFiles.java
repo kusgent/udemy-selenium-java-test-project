1c0e67d7abbcce7b39725ea3b5e554c687b2f3d5
Kustov Ilya <kusgent@yandex.ru>
2022-01-22 16:23:01 +0300

    Running tests in different browsers

--- src/main/java/UploadFiles.java
+++ src/main/java/UploadFiles.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 
@@ -8,8 +10,13 @@ public class UploadFiles {
     static WebDriver driver;
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver", "C:\\geckodriver\\geckodriver.exe");
+        System.setProperty("webdriver.edge.driver", "C:\\edgedriver_win64\\msedgedriver.exe");
 
-        driver = new ChromeDriver();
+//        driver = new ChromeDriver();
+//        driver = new FirefoxDriver();
+        driver = new EdgeDriver();
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
