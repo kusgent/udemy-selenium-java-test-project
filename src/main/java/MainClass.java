17eeaccee51fdd3e0a58e3e600900751ebc877b8
Kustov Ilya <kusgent@yandex.ru>
2022-01-23 21:10:56 +0300

    Page Object: trial run

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,21 @@
-public class MainClass {
-    public static void main(String[] args) {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
+public class MainClass {
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://github.com/");
+
+        MainPage mainPage = new MainPage(driver);
+        mainPage.register("test@test.com");
     }
 }
